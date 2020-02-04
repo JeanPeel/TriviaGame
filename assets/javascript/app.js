d018cd334ab8bbe29b7620e040ddb915c224a8c6
Jean Peel <Jean.Peel.246@gmail.com>
2020-02-04 16:30:51 -0700

    psudo coding outline

---
+++ assets/javascript/app.js
@@ -0,0 +1,61 @@
+
+var question1 = "";
+var question2 = "";
+var question3 = "";
+var question4 = "";
+
+var answer1 = "";
+var answer2 = "";
+var answer3 = "";
+var answer4 = "";
+
+var questions = (question1, question2, question3, question4);
+var answers = (answer1, answer2, answer3, answer4);
+
+var timer = ();
+
+
+//Start Btn 
+    //Imeediate, Action, Function, Method
+    //Starts the Timer
+    //Displays the Questions
+//Timer
+    //Imeediate & Delayed, Variable, Method & Property, Information and Thing
+    //Counts Down
+    //Triggers Loose condition if reaches 0
+//Loose Condition
+    //Imeedatly after triggered,  Variable, Inofrmation, Property
+    //Displays you "loose" and either time is out or..
+    //Corrected answers
+    //Displays reset button
+//Question 1
+    // Information, Delayed, Variable, Property
+//Question 2
+    // Information, Delayed, Variable, Property
+//Question 3
+    // Information, Delayed, Variable, Property
+//Question 4
+    // Information, Delayed, Variable, Property
+//Answer 1
+    // Information, Delayed, Variable, Property
+//Answer 2
+    // Information, Delayed, Variable, Property
+//Answer 3
+    // Information, Delayed, Variable, Property
+//Answer 4
+    // Information, Delayed, Variable, Property
+//Submit
+    //Action, Imeediate, function, method
+    //Compares answers to correct answers with If/ Else Statements
+        // If all the questions are true then win Condition is triggered
+        // If any of the questions are false then loose condition is triggered
+// Win condition 
+    // Action, Imeediate, function method
+    // Displays "You won" and correct answers.
+    // Displays reset button
+// Reset
+    // Resets timer
+    // Resets scores
+    // Displays Start screen and Start Button
+
+// some document writing and console logging is needed also.
